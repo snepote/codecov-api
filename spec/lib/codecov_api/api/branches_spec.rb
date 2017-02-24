ac216c9a0c452968fdbee35bf44474c5301e8783
Sebastian Nepote <sebastian.nepote@helpling.com>
2017-02-24 12:44:29 +0100

    webmock all the specs and fixes rubocop

--- spec/lib/codecov_api/api/branches_spec.rb
+++ spec/lib/codecov_api/api/branches_spec.rb
@@ -6,26 +6,25 @@ RSpec.describe CodecovApi::Api::Branches do
   let(:owner) { 'owner' }
   let(:repo) { 'repo' }
   let(:base_url) { "https://codecov.io/api/gh/#{owner}/#{repo}" }
-  let(:auth_token) { 'some_valid_auth_token' }
   let(:json) { '{"param": "value"}' }
   subject do
     described_class.new(owner, repo)
   end
 
   before(:example) do
-    allow(subject).to receive(:auth_token).and_return(auth_token)
+    allow(subject).to receive(:auth_token).and_return('some_valid_auth_token')
   end
 
   describe '#list' do
     it 'should request a valid url' do
-      stub_request(:get, "#{base_url}/branches").to_return(status: 200, body: json)
+      stub_request(:get, "#{base_url}/branches").to_return(body: json)
       expect(subject.list).to eq json
     end
   end
 
   describe '#branch' do
     it 'should request a valid url' do
-      stub_request(:get, "#{base_url}/branch/some_branch").to_return(status: 200, body: json)
+      stub_request(:get, "#{base_url}/branch/some_branch").to_return(body: json)
       expect(subject.get('some_branch')).to eq(json)
     end
   end
