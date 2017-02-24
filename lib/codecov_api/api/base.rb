b02390e4824b1fb9bb84684796a27621eacd7302
Sebastian Nepote <sebastian.nepote@helpling.com>
2017-02-24 11:59:23 +0100

    Removes verbosity and URI obsolete escape method

--- lib/codecov_api/api/base.rb
+++ lib/codecov_api/api/base.rb
@@ -26,8 +26,6 @@ module CodecovApi
       end
 
       def response(url, method: :get)
-        url = URI.escape(url)
-        puts "#{method.to_s.upcase} #{url}"
         RestClient::Request.new(
           method: method,
           url: url,
